c731174894486b9f3a95206eeb336f1077137b6c
Saibejjani <64737059+Saibejjani@users.noreply.github.com>
2023-05-26 18:54:23 +0530

    final commits are made

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import Editor from './Editor';
+import useLocalStorage from '../hooks/useLocalStorage';
 function App() {
-  return 'Hello world';
+  const [html, setHtml] = useLocalStorage('html', '');
+  const [css, setCss] = useLocalStorage('css', '');
+  const [JS, setJS] = useLocalStorage('js', '');
+  const [srcDoc, setSrcDoc] = useState('');
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`<html>
+      <body>
+      ${html}
+      </body>
+      <style>${css}</style>
+      <script>${JS}</script>
+      </html>`);
+    }, 250);
+  }, [html, css, JS]);
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={JS}
+          onChange={setJS}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          frameBorder="0"
+          sandbox="allow-scripts"
+          width="100%"
+          height="100%"
+        ></iframe>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Editor.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/css/css';
+import { Controlled as ControlledEditor } from 'react-codemirror2';
+
+const Editor = (props) => {
+  const [open, setOpen] = useState(true);
+  const { displayName, value, language, onChange } = props;
+  const handleChange = (editor, data, value) => {
+    onChange(value);
+  };
+  return (
+    <div className={`editor-container ${open ? '' : 'collapsed'}`}>
+      <div className="editor-title">
+        {displayName}
+        <button
+          onClick={() => {
+            setOpen((prev) => !prev);
+          }}
+        >
+          {open ? 'X' : 'O'}
+        </button>
+      </div>
+      <ControlledEditor
+        onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          lineNumbers: true,
+          theme: 'material',
+        }}
+      />
+    </div>
+  );
+};
+
+export default Editor;
